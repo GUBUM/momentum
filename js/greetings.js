dbdfcfa2e5e6b2489f0d1b52026760a89dad2d20
GUBUM <044__@naver.com>
2021-10-14 18:37:58 +0900

    Add files via upload

--- js/greetings.js
+++ js/greetings.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const todo = document.querySelector("#todo");
+const h1 = greeting.querySelector("h1");
+const signOut = greeting.querySelector("input");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+function onSingoutSubmit(){
+    localStorage.clear(USERNAME_KEY);
+    location.reload(true);
+}
+signOut.addEventListener("click",onSingoutSubmit);
 function onLoginSubmit(event){
     event.preventDefault();
 
@@ -14,8 +22,9 @@ function onLoginSubmit(event){
     paintGreetings(username);
 }
 function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
+    h1.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    todo.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,16 @@
 const API_KEY ="3dd3e7ff268ec8bfdeec7fe0a2ab2687";
+let weatherIcon = { 
+    '01' : 'fas fa-sun', 
+    '02' : 'fas fa-cloud-sun', 
+    '03' : 'fas fa-cloud', 
+    '04' : 'fas fa-cloud-meatball', 
+    '09' : 'fas fa-cloud-sun-rain', 
+    '10' : 'fas fa-cloud-showers-heavy',
+    '11' : 'fas fa-poo-storm',
+    '13' : 'far fa-snowflake', 
+    '50' : 'fas fa-smog' 
+};
+// https://openweathermap.org/    openweather API
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
@@ -7,10 +19,14 @@ function onGeoOk(position){
     fetch(url)
     .then(response => response.json())
     .then(data =>{
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+        const icon = document.querySelector("#weather img");
+        
+        icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        // `${weatherIcon[(data.weather[0].icon).substr(0,2)]}`
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`; 
+        weather.innerText = `${data.weather[0].main}\n${data.main.temp} °C`; 
     });
 }
 function onGeoError(){
